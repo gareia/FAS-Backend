56ff477aa0727e2ded5927068b077c1ff00b0902
Grecia <greciaguerreroa@gmail.com>
2022-04-21 12:03:49 -0500

    upload base project

---
+++ src/main/java/com/acme/elegant/ElegantApplication.java
@@ -0,0 +1,13 @@
+package com.acme.elegant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElegantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElegantApplication.class, args);
+    }
+
+}
